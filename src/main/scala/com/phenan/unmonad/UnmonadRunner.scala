eee87efc95c060e5c159ec1c4431f0cc137996c7
Kazuhiro Ichikawa <kazuhiro.ichikawa@linecorp.com>
2023-01-29 21:56:27 +0900

    use Monad.tailRecM

--- src/main/scala/com/phenan/unmonad/UnmonadRunner.scala
+++ src/main/scala/com/phenan/unmonad/UnmonadRunner.scala
@@ -3,16 +3,14 @@ package com.phenan.unmonad
 import cats.Monad
 
 class UnmonadRunner [F[_], M[_]: Monad](runner: [T] => F[T] => M[T]) {
-  def apply[R](logic: UnmonadContext[F] ?=> R): M[R] = runWithContext(UnmonadContext.initialContext[F], logic)
-
-  private def runWithContext[R](context: UnmonadContext[F], logic: UnmonadContext[F] ?=> R): M[R] = {
-    try {
-      Monad[M].pure[R](logic(using context))
-    } catch {
-      case e: UnmonadRollbackException[F, _] =>
-        Monad[M].flatMap(runner(e.action)) { t =>
-          runWithContext(context.addActionResult(t), logic)
-        }
+  def apply[R](logic: UnmonadContext[F] ?=> R): M[R] = {
+    Monad[M].tailRecM[UnmonadContext[F], R](UnmonadContext.initialContext[F]) { context =>
+      try {
+        Monad[M].pure(Right(logic(using context)))
+      } catch {
+        case e: UnmonadRollbackException[F, _] =>
+          Monad[M].map(runner(e.action)) { t => Left(context.addActionResult(t)) }
+      }
     }
   }
 }
