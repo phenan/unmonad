ab9f697caad22de553377d668f86832098cfba56
Kazuhiro Ichikawa <kazuhiro.ichikawa@linecorp.com>
2023-01-31 21:11:33 +0900

    explicitly write implicit parameter of UnmonadRunner

--- src/main/scala/com/phenan/unmonad/UnmonadRunner.scala
+++ src/main/scala/com/phenan/unmonad/UnmonadRunner.scala
@@ -3,14 +3,14 @@ package com.phenan.unmonad
 import cats.arrow.FunctionK
 import cats.Monad
 
-class UnmonadRunner [F[_], M[_]: Monad](runner: FunctionK[F, M]) {
+class UnmonadRunner [F[_], M[_]](runner: FunctionK[F, M])(implicit monad: Monad[M]) {
   def run[R](logic: UnmonadContext[F] => R): M[R] = {
-    Monad[M].tailRecM[UnmonadContext[F], R](UnmonadContext.initialContext[F]) { context =>
+    monad.tailRecM[UnmonadContext[F], R](UnmonadContext.initialContext[F]) { context =>
       try {
-        Monad[M].pure(Right(logic(context)))
+        monad.pure(Right(logic(context)))
       } catch {
         case e: UnmonadRollbackException[F, _] =>
-          Monad[M].map(runner(e.action)) { t => Left(context.addActionResult(t)) }
+          monad.map(runner(e.action)) { t => Left(context.addActionResult(t)) }
       }
     }
   }
