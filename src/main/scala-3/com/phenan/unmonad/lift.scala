5d44bfc7b3e93b95546d87c80f0cf7d9637ba917
Kazuhiro Ichikawa <kazuhiro.ichikawa@linecorp.com>
2023-02-01 20:51:20 +0900

    rewrite lift / rewrite kvsSyntaxTest to directly use unmonad

--- src/main/scala-3/com/phenan/unmonad/lift.scala
+++ src/main/scala-3/com/phenan/unmonad/lift.scala
@@ -2,9 +2,7 @@ package com.phenan.unmonad
 
 import cats.arrow.FunctionK
 
-object lift {
-  def apply[F[_]]: Lifter[F, F] = new Lifter[F, F](FunctionK.id)
-}
+def lift[F[_]]: Lifter[F, F] = new Lifter[F, F](FunctionK.id)
 
 def unlift[F[_], T](action: F[T]): UnmonadContext[F] ?=> T = {
   summon[UnmonadContext[F]].action(action)
